ee44d1e51ce6083d85f320a3dc615e3037e8007d
Timothy Pratley <timothypratley@gmail.com>
2015-05-25 22:43:25 -0700

    google analytics

--- src/algopop/leaderboardx/pages.clj
+++ src/algopop/leaderboardx/pages.clj
@@ -28,7 +28,13 @@
     [:link {:rel "stylesheet" :href "css/site.css"}]
     [:link {:rel "stylesheet" :href "//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"}]
     [:script {:src "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"}]
-    [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"}]]
+    [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"}]
+    [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-40336415-3', 'auto');
+ga('send', 'pageview');"]]
    [:body
     [:div {:id "app"}]
     [:script {:src "js/app.js" :type "text/javascript"}]]))
