7ad2c81daf530c1e6474741378f38f8f7e8f7c8e
Timothy Pratley <timothypratley@gmail.com>
2020-08-30 18:47:40 -0700

    fix deploy relative path

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -2,7 +2,7 @@
 set -e
 cd $(dirname $0)
 lein with-profile uberjar do clean, cljsbuild once
-cd resources/public
+cd ../resources/public
 mv index.html index.html.bak
 awk -v n=3 -v s='<h3>This is the development version of LeaderboardX.<br>If you would prefer the stable version, please visit <a href="http://leaderboardx.herokuapp.com/">http://leaderboardx.herokuapp.com</a></h3>' 'NR == n {print s} {print}' index.html.bak > index.html
 git init
