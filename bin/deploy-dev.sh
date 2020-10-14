c0ae7ff9f30cacf5f466cc4613f2aeb801f336bf
Timothy Pratley <timothypratley@gmail.com>
2020-10-14 06:13:49 -0700

    bump

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -4,11 +4,11 @@ cd $(dirname $0)
 lein with-profile uberjar do clean, cljsbuild once
 cd ../resources/public
 mv index.html index.html.bak
-awk -v n=3 -v s='<h3>This is the development version of LeaderboardX.<br>If you would prefer the stable version, please visit <a href="http://leaderboardx.herokuapp.com/">http://leaderboardx.herokuapp.com</a></h3>' 'NR == n {print s} {print}' index.html.bak > index.html
+awk -v n=3 -v s='<h3>This is the development version of LeaderboardX.<br>If you would prefer the stable version, please visit <a href="https://leaderboardx.herokuapp.com/">https://leaderboardx.herokuapp.com</a></h3>' 'NR == n {print s} {print}' index.html.bak > index.html
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "git@github.com:timothypratley/leaderboardx.git" master:gh-pages
 mv index.html.bak index.html
 rm -fr .git
-echo http://timothypratley.github.io/leaderboardx
+echo https://timothypratley.github.io/leaderboardx
