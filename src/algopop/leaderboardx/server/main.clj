4be4567de2bae95fbc8fab6689384f6eb46e9ba6
Timothy Pratley <timothypratley@gmail.com>
2021-08-06 12:23:57 -0700

    redirect heroku to github pages

--- src/algopop/leaderboardx/server/main.clj
+++ src/algopop/leaderboardx/server/main.clj
@@ -5,13 +5,11 @@
     [compojure.handler :refer [site]]
     [compojure.core :refer [defroutes GET POST DELETE ANY context]]
     [environ.core :refer [env]]
-    [ring.util.response :refer [content-type resource-response]])
+    [ring.util.response :refer [content-type resource-response redirect]])
   (:gen-class))
 
 (defroutes all-routes
-  (GET "/" [] (content-type (resource-response "index.html" {:root "public"}) "text/html"))
-  (resources "/")
-  (not-found "<p>Page not found.</p>"))
+  (redirect "https://timothypratley.github.io/leaderboardx/"))
 
 (defn -main [& args]
   (run-server (site #'all-routes) {:port (Integer. (env :port 3000))})
